ee7baa0e776797e6e7eff660bbfc9e8c5ca6f614
GoForBroke <go.for.broke1006@gmail.com>
2023-08-21 01:38:36 +0300

    Add old resources

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 #include <Compositor/OgreCompositorManager2.h>
 #include <OgreMeshManager2.h>
 #include <OgreSharedPtr.h>
+#include <OgreLight.h>
+#include <OgreMesh.h>
+#include <OgreMesh2.h>
 #include <OgreItem.h>
 #include <OgrePlatformInformation.h>
 
@@ -44,29 +47,92 @@ public:
 };
 
 // V2 mesh loading
-Ogre::Item *ogreLoadMesh(Ogre::SceneManager *const sceneManager, const char *name) {
-    Ogre::MeshPtr v2Mesh = Ogre::MeshManager::getSingleton().load(
-            name, Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
+Ogre::Item *ogreLoadMesh(Ogre::SceneManager *const sceneManager, const std::string &filepath) {
+//    Ogre::MeshPtr v2Mesh = Ogre::MeshManager::getSingleton().load(
+//            name, Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
+//    Ogre::Item *item = sceneManager->createItem(
+//            name, Ogre::ResourceGroupManager::AUTODETECT_RESOURCE_GROUP_NAME, Ogre::SCENE_DYNAMIC);
+//    return item;
+
+    bool halfPosition = true;
+    bool halfUVs = true;
+    bool useQtangents = true;
+
+    Ogre::v1::MeshPtr v1Mesh = Ogre::v1::MeshManager::getSingleton().load(
+            filepath, Ogre::ResourceGroupManager::AUTODETECT_RESOURCE_GROUP_NAME,
+            Ogre::v1::HardwareBuffer::HBU_STATIC, Ogre::v1::HardwareBuffer::HBU_STATIC);
+    //Create a v2 mesh to import to, with a different name.
+    Ogre::MeshPtr v2Mesh = Ogre::MeshManager::getSingleton().createByImportingV1(
+            filepath + " Imported", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
+            v1Mesh.get(), halfPosition, halfUVs, useQtangents);
+    v2Mesh->load();
+    v1Mesh->unload();
+
     Ogre::Item *item = sceneManager->createItem(
-            name, Ogre::ResourceGroupManager::AUTODETECT_RESOURCE_GROUP_NAME, Ogre::SCENE_DYNAMIC);
+            filepath + " Imported", Ogre::ResourceGroupManager::AUTODETECT_RESOURCE_GROUP_NAME, Ogre::SCENE_DYNAMIC);
     return item;
 }
 
 Ogre::SceneNode *ogreNodeFromItem(Ogre::SceneManager *const sceneManager, Ogre::Item *meshHandle) {
-    Ogre::SceneNode *newSceneNode = nullptr;
-    Ogre::SceneNode *rootSceneNode = sceneManager->getRootSceneNode();
-    if (rootSceneNode) {
-        Ogre::SceneNode *sceneNode = ((Ogre::SceneNode *) rootSceneNode->createChild());
-        if (sceneNode) {
-            sceneNode->attachObject(meshHandle);
-            newSceneNode = sceneNode;
-        }
-    }
-    return newSceneNode;
+//    Ogre::SceneNode *newSceneNode = nullptr;
+//    Ogre::SceneNode *rootSceneNode = sceneManager->getRootSceneNode();
+//    if (rootSceneNode) {
+//        Ogre::SceneNode *sceneNode = ((Ogre::SceneNode *) rootSceneNode->createChild());
+//        if (sceneNode) {
+//            sceneNode->attachObject(meshHandle);
+//            newSceneNode = sceneNode;
+//        }
+//    }
+//    return newSceneNode;
+
+    Ogre::SceneNode *sceneNode = sceneManager->getRootSceneNode(Ogre::SCENE_DYNAMIC)->
+            createChildSceneNode(Ogre::SCENE_DYNAMIC);
+    sceneNode->attachObject(meshHandle);
+    sceneNode->scale(0.1f, 0.1f, 0.1f);
 }
 
 int entryPoint() {
     auto *gOgreRoot = OGRE_NEW Ogre::Root("plugins.cfg", "resources2.cfg", "ogre.cfg", "App Name");
+
+    Ogre::RenderSystem *renderSystem = gOgreRoot->getRenderSystemByName("OpenGL 3+ Rendering Subsystem");
+    if (!(renderSystem)) {
+        printf("Render system not found!\n");
+        return EXIT_FAILURE;
+    }
+    renderSystem->setConfigOption("Full Screen", "No");
+    renderSystem->setConfigOption("Video Mode", "800 x 600");
+    renderSystem->setConfigOption("sRGB Gamma Conversion", "Yes");
+    gOgreRoot->setRenderSystem(renderSystem);
+
+    auto *gOgreWindow = gOgreRoot->initialise(true, "Hello Ogre next");
+
+    const size_t numThreads = std::max<uint32_t>(1, Ogre::PlatformInformation::getNumLogicalCores());
+    auto *gSceneManager = gOgreRoot->createSceneManager(Ogre::ST_GENERIC, numThreads, Ogre::BLANKSTRING);
+
+    Ogre::Camera *camera = gSceneManager->createCamera("Main Camera");
+    camera->setPosition(Ogre::Vector3(100, 100, 100));
+    camera->lookAt(Ogre::Vector3(0, 0, 0));
+    camera->setNearClipDistance(0.2f);
+    camera->setFarClipDistance(1000.0f);
+    camera->setAutoAspectRatio(true);
+
+    // without light we would just get a black screen
+//    Ogre::Light *light = gSceneManager->createLight();
+//    Ogre::SceneNode *lightNode = gSceneManager->getRootSceneNode()->createChildSceneNode();
+//    lightNode->setPosition(0, 200, 0);
+//    lightNode->attachObject(light);
+
+//    Ogre::Light *light = gSceneManager->createLight();
+//    Ogre::SceneNode *lightNode = gSceneManager->getRootSceneNode()->createChildSceneNode();
+//    lightNode->attachObject(light);
+//    light->setPowerScale(Ogre::Math::PI); //Since we don't do HDR, counter the PBS' division by PI
+//    light->setType(Ogre::Light::LT_DIRECTIONAL);
+//    light->setDirection(Ogre::Vector3(-1, -1, -1).normalisedCopy());
+
+    auto *compositorManager = gOgreRoot->getCompositorManager2();
+    const Ogre::String workspaceName = "Main Workspace";
+    const Ogre::ColourValue backgroundColour(0.0302f, 0.03f, 0.03f);
+    compositorManager->createBasicWorkspaceDef(workspaceName, backgroundColour, Ogre::IdString());
 
     //
 
@@ -87,46 +153,11 @@ int entryPoint() {
         }
     }
 
-    Ogre::ResourceGroupManager::getSingleton().initialiseAllResourceGroups(false);
+    Ogre::ResourceGroupManager::getSingleton().initialiseAllResourceGroups(true);
 
     //
 
-    Ogre::RenderSystem *renderSystem =
-            gOgreRoot->getRenderSystemByName("OpenGL 3+ Rendering Subsystem");
-    if (!(renderSystem)) {
-        printf("Render system not found!\n");
-        return EXIT_FAILURE;
-    }
-    renderSystem->setConfigOption("Full Screen", "No");
-    renderSystem->setConfigOption("Video Mode", "800 x 600");
-    renderSystem->setConfigOption("sRGB Gamma Conversion", "Yes");
-    gOgreRoot->setRenderSystem(renderSystem);
-
-    auto *gOgreWindow = gOgreRoot->initialise(true, "Hello Ogre next");
-
-    const size_t numThreads = std::max<uint32_t>(1, Ogre::PlatformInformation::getNumLogicalCores());
-    auto *gSceneManager = gOgreRoot->createSceneManager(Ogre::ST_GENERIC, numThreads, Ogre::BLANKSTRING);
-
-    Ogre::Camera *camera = gSceneManager->createCamera("Main Camera");
-    camera->setPosition(Ogre::Vector3(0, 5, 15));
-    camera->lookAt(Ogre::Vector3(0, 0, 0));
-    camera->setNearClipDistance(0.2f);
-    camera->setFarClipDistance(1000.0f);
-    camera->setAutoAspectRatio(true);
-
-    // without light we would just get a black screen
-//    Ogre::Light *light = gSceneManager->createLight();
-//    Ogre::SceneNode *lightNode = gSceneManager->getRootSceneNode()->createChildSceneNode();
-//    lightNode->setPosition(0, 10, 15);
-//    lightNode->attachObject(light);
-
-    auto *compositorManager = gOgreRoot->getCompositorManager2();
-    const Ogre::String workspaceName = "Main Workspace";
-    const Ogre::ColourValue backgroundColour(0.0302f, 0.03f, 0.03f);
-    compositorManager->createBasicWorkspaceDef(workspaceName, backgroundColour, Ogre::IdString());
-
-
-    const std::string &barrelMeshPath = "Barrel.mesh";
+    const std::string &barrelMeshPath = "Sinbad.mesh";
 
     bool isBarrelMeshExists = Ogre::ResourceGroupManager::getSingleton().resourceExists("General", barrelMeshPath);
     if (!isBarrelMeshExists) {
@@ -134,7 +165,7 @@ int entryPoint() {
         return EXIT_FAILURE;
     }
 
-    Ogre::Item *item = ogreLoadMesh(gSceneManager, barrelMeshPath.c_str());
+    Ogre::Item *item = ogreLoadMesh(gSceneManager, barrelMeshPath);
     Ogre::SceneNode *pBarrelNode = ogreNodeFromItem(gSceneManager, item);
 
     MyWindowEventListener g_myWindowEventListener;
@@ -149,6 +180,9 @@ int entryPoint() {
             break;
         }
     }
+
+    OGRE_DELETE pBarrelNode;
+    OGRE_DELETE item;
 
     Ogre::WindowEventUtilities::removeWindowEventListener(gOgreWindow, &g_myWindowEventListener);
     OGRE_DELETE (gOgreRoot);
