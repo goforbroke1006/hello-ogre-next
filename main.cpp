5c70d02953424f6533e47906744ad353c4a14e09
GoForBroke <go.for.broke1006@gmail.com>
2023-08-20 03:37:05 +0300

    Add resources loading

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
 #include <Ogre.h>
+#include <OgreWindow.h>
+#include <Compositor/OgreCompositorManager2.h>
+#include <OgreMeshManager2.h>
+#include <OgreSharedPtr.h>
+#include <OgreItem.h>
+#include <OgrePlatformInformation.h>
 
 int entryPoint();
 
@@ -31,14 +37,59 @@ static bool g_ogre_window_should_quit = false;
 
 class MyWindowEventListener : public Ogre::WindowEventListener {
 public:
-    virtual void windowClosed(Ogre::Window *window) {
+    void windowClosed(Ogre::Window *window) override {
         printf("Window closed!\n");
         g_ogre_window_should_quit = true;
     }
 };
 
+// V2 mesh loading
+Ogre::Item *ogreLoadMesh(Ogre::SceneManager *const sceneManager, const char *name) {
+    Ogre::MeshPtr v2Mesh = Ogre::MeshManager::getSingleton().load(
+            name, Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
+    Ogre::Item *item = sceneManager->createItem(
+            name, Ogre::ResourceGroupManager::AUTODETECT_RESOURCE_GROUP_NAME, Ogre::SCENE_DYNAMIC);
+    return item;
+}
+
+Ogre::SceneNode *ogreNodeFromItem(Ogre::SceneManager *const sceneManager, Ogre::Item *meshHandle) {
+    Ogre::SceneNode *newSceneNode = nullptr;
+    Ogre::SceneNode *rootSceneNode = sceneManager->getRootSceneNode();
+    if (rootSceneNode) {
+        Ogre::SceneNode *sceneNode = ((Ogre::SceneNode *) rootSceneNode->createChild());
+        if (sceneNode) {
+            sceneNode->attachObject(meshHandle);
+            newSceneNode = sceneNode;
+        }
+    }
+    return newSceneNode;
+}
+
 int entryPoint() {
-    auto *gOgreRoot = OGRE_NEW Ogre::Root();
+    auto *gOgreRoot = OGRE_NEW Ogre::Root("plugins.cfg", "resources2.cfg", "ogre.cfg", "App Name");
+
+    //
+
+    Ogre::ConfigFile cf;
+    cf.load("resources.cfg");
+
+    Ogre::ConfigFile::SectionIterator seci = cf.getSectionIterator();
+
+    Ogre::String secName, typeName, archName;
+    while (seci.hasMoreElements()) {
+        secName = seci.peekNextKey();
+        Ogre::ConfigFile::SettingsMultiMap *settings = seci.getNext();
+        Ogre::ConfigFile::SettingsMultiMap::iterator i;
+        for (i = settings->begin(); i != settings->end(); ++i) {
+            typeName = i->first;
+            archName = i->second;
+            Ogre::ResourceGroupManager::getSingleton().addResourceLocation(archName, typeName, secName);
+        }
+    }
+
+    Ogre::ResourceGroupManager::getSingleton().initialiseAllResourceGroups(false);
+
+    //
 
     Ogre::RenderSystem *renderSystem =
             gOgreRoot->getRenderSystemByName("OpenGL 3+ Rendering Subsystem");
@@ -51,19 +102,40 @@ int entryPoint() {
     renderSystem->setConfigOption("sRGB Gamma Conversion", "Yes");
     gOgreRoot->setRenderSystem(renderSystem);
 
-    auto *gOgreWindow = gOgreRoot->initialise(true, "OgreStartProject");
+    auto *gOgreWindow = gOgreRoot->initialise(true, "Hello Ogre next");
 
-    const size_t numThreads = 1u;
-    auto *gSceneManager = gOgreRoot->createSceneManager(Ogre::ST_GENERIC, numThreads, "SceneManager");
+    const size_t numThreads = std::max<uint32_t>(1, Ogre::PlatformInformation::getNumLogicalCores());
+    auto *gSceneManager = gOgreRoot->createSceneManager(Ogre::ST_GENERIC, numThreads, Ogre::BLANKSTRING);
 
     Ogre::Camera *camera = gSceneManager->createCamera("Main Camera");
-    {
-        camera->setPosition(Ogre::Vector3(0, 5, 15));
-        camera->lookAt(Ogre::Vector3(0, 0, 0));
-        camera->setNearClipDistance(0.2f);
-        camera->setFarClipDistance(1000.0f);
-        camera->setAutoAspectRatio(true);
+    camera->setPosition(Ogre::Vector3(0, 5, 15));
+    camera->lookAt(Ogre::Vector3(0, 0, 0));
+    camera->setNearClipDistance(0.2f);
+    camera->setFarClipDistance(1000.0f);
+    camera->setAutoAspectRatio(true);
+
+    // without light we would just get a black screen
+//    Ogre::Light *light = gSceneManager->createLight();
+//    Ogre::SceneNode *lightNode = gSceneManager->getRootSceneNode()->createChildSceneNode();
+//    lightNode->setPosition(0, 10, 15);
+//    lightNode->attachObject(light);
+
+    auto *compositorManager = gOgreRoot->getCompositorManager2();
+    const Ogre::String workspaceName = "Main Workspace";
+    const Ogre::ColourValue backgroundColour(0.0302f, 0.03f, 0.03f);
+    compositorManager->createBasicWorkspaceDef(workspaceName, backgroundColour, Ogre::IdString());
+
+
+    const std::string &barrelMeshPath = "Barrel.mesh";
+
+    bool isBarrelMeshExists = Ogre::ResourceGroupManager::getSingleton().resourceExists("General", barrelMeshPath);
+    if (!isBarrelMeshExists) {
+        std::cerr << "ERROR: resource not found " << barrelMeshPath << std::endl;
+        return EXIT_FAILURE;
     }
+
+    Ogre::Item *item = ogreLoadMesh(gSceneManager, barrelMeshPath.c_str());
+    Ogre::SceneNode *pBarrelNode = ogreNodeFromItem(gSceneManager, item);
 
     MyWindowEventListener g_myWindowEventListener;
     Ogre::WindowEventUtilities::addWindowEventListener(gOgreWindow, &g_myWindowEventListener);
